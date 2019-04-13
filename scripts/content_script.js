42d43de24eba623fb03608c54e0a3f7fed833850
Alin Pasol <programming.alinpasol@gmx.com>
2019-04-13 21:10:58 +0300

    Change the name of the attribute with the shortened URL

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -19,7 +19,7 @@ function revealLinks() {
 ${index + 1}.href             :${link.href}
 ${index + 1}.data-expanded-url:${link.getAttribute("data-expanded-url")}
 ${index + 1}.title            :${link.title}`);*/    // for debugging
-      link.setAttribute("shortened-url", link.href);
+      link.setAttribute("data-shortened-url", link.href);
       link.href = link.getAttribute("data-expanded-url");
       //console.log(link);    // for debugging
     }
