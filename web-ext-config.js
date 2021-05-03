bad55b53313c5cd3d5544bce292ff6261f945f41
Alin Pasol <programming.alinpasol@gmx.com>
2021-05-04 01:34:58 +0300

    chore: add Web-ext as a development dependency

---
+++ web-ext-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ignoreFiles: [
+    'package.json',
+    'package-lock.json',
+    'web-ext-config.js'
+  ],
+  run: {
+    browserConsole: true,
+    firefox: "/opt/firefox-aurora/firefox",
+    startUrl: [
+      "about:debugging#/runtime/this-firefox",
+      "https://twitter.com/firefox"
+    ]
+  }
+};
